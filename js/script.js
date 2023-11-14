dbc8725a3a357def4e398597ff01bd5927780c50
Bdubs <bmwebb9@gmail.com>
2023-11-14 14:07:19 -0500

    added response.statusText to the error handler so when troubleshooting we know exactly what the status code is, removed the event handlers which showed alerts,removed the both joke function as it was preventing new jokes from displaying on click, created a new function which adds the joke in based on thier respective apis and updated our jquery method.

--- js/script.js
+++ js/script.js
@@ -1,75 +1,46 @@
-var jokeUrl1 = "https://v2.jokeapi.dev/joke/Programming,Spooky,Christmas?blacklistFlags=religious,racist,sexist,explicit&format=txt&type=single&amount=1";
-var jokeUrl2 = 'https://icanhazdadjoke.com/'
-
-var bothJokes = [];
-
-$(".dad").on("click", function() {
-  alert("Joke clicked!");
-});
-
-$(".prog").on("click", function(){
-  alert("Joke clicked");
-});
-
-$("#favorButton").on("click", function(){
-  alert("Button clicked")
-})
-
-$(".contrast").on("click", function(){
+  var jokeUrl1 = "https://v2.jokeapi.dev/joke/Programming,Spooky,Christmas?blacklistFlags=religious,racist,sexist,explicit&format=txt&type=single&amount=1";
+  var jokeUrl2 = 'https://icanhazdadjoke.com/';
   
-})
-
-
-
-function fetchJokes(apiUrl) {
-  return fetch(apiUrl, {
-    headers: {
-      Accept: "text/plain"
-    }
-  })
-    .then(function (response) {
-      if (response.ok) {
-        return response.text();
-      } else {
-        console.log('Network response was not ok');
-      
+  var bothJokes = [];
+  
+  function fetchJoke(apiUrl) {
+    return fetch(apiUrl, {
+      headers: {
+        Accept: "text/plain"
       }
     })
-    .then(function (jokes) {
-      bothJokes.push(jokes);
-    })
-    .catch(function () {
-      console.log('Error:');
-    });
-}
-
-fetchJokes(jokeUrl1)
-  .then(function () {
-    return fetchJokes(jokeUrl2);
-  })
-  .then(function () {
-    console.log('Both jokes:', bothJokes);
-
-    for (let i = 0; i < bothJokes.length; i++) {
-      console.log(bothJokes[i]);
-    }
-  })
-
-  .catch(function () {
-    console.error('An error occurred:');
+      .then(function (response) {
+        if (response.ok) {
+          return response.text();
+        } else {
+          console.error('Network response was not ok:', response.statusText);
+          throw new Error('Failed to fetch joke');
+        }
+      })
+      .catch(function (error) {
+        console.error('Error:', error.message);
+        throw error;
+      });
+  }
+  
+  function displayJoke(joke) {
+    $('#jokeText').text(joke);
+  }
+  
+  function displayJoke(apiUrl) {
+    fetchJoke(apiUrl)
+      .then(function (joke) {
+        displayJoke(joke);
+      })
+      .catch(function (error) {
+        console.error('An error occurred:', error.message);
+      });
+  }
+  
+  $('.dad').on('click', function () {
+    displayJoke(jokeUrl2);
   });
-
-
-  $('#dadJoke').on('click', function () {
-    $('#jokeText').text(bothJokes[1]);
+  
+  $('.prog').on('click', function () {
+    displayJoke(jokeUrl1);
   });
-  $('#progJoke').on('click', function () {
-    $('#jokeText').text(bothJokes[0]);
-  });
-  
-  
-  
-  
-  
-function getDadJoke(){
-}
